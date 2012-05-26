1be6b97af324490b1f667ef844fc34588841582e
Asko Soukka <asko.soukka@iki.fi>
2012-05-26 14:03:55 +0300

    Moved code under src. Added test buildout.
    --HG--
    rename : collective/__init__.py => src/collective/__init__.py
    rename : collective/pfg/__init__.py => src/collective/pfg/__init__.py
    rename : collective/pfg/dexterity/__init__.py => src/collective/pfg/dexterity/__init__.py
    rename : collective/pfg/dexterity/adapter.py => src/collective/pfg/dexterity/adapter.py
    rename : collective/pfg/dexterity/config.py => src/collective/pfg/dexterity/config.py
    rename : collective/pfg/dexterity/configure.zcml => src/collective/pfg/dexterity/configure.zcml
    rename : collective/pfg/dexterity/interfaces.py => src/collective/pfg/dexterity/interfaces.py
    rename : collective/pfg/dexterity/locales/collective.pfg.dexterity.pot => src/collective/pfg/dexterity/locales/collective.pfg.dexterity.pot
    rename : collective/pfg/dexterity/locales/en/LC_MESSAGES/collective.pfg.dexterity.po => src/collective/pfg/dexterity/locales/en/LC_MESSAGES/collective.pfg.dexterity.po
    rename : collective/pfg/dexterity/locales/fi/LC_MESSAGES/collective.pfg.dexterity.po => src/collective/pfg/dexterity/locales/fi/LC_MESSAGES/collective.pfg.dexterity.po
    rename : collective/pfg/dexterity/locales/fr/LC_MESSAGES/collective.pfg.dexterity.po => src/collective/pfg/dexterity/locales/fr/LC_MESSAGES/collective.pfg.dexterity.po
    rename : collective/pfg/dexterity/profiles/default/factorytool.xml => src/collective/pfg/dexterity/profiles/default/factorytool.xml
    rename : collective/pfg/dexterity/profiles/default/metadata.xml => src/collective/pfg/dexterity/profiles/default/metadata.xml
    rename : collective/pfg/dexterity/profiles/default/propertiestool.xml => src/collective/pfg/dexterity/profiles/default/propertiestool.xml
    rename : collective/pfg/dexterity/profiles/default/types.xml => src/collective/pfg/dexterity/profiles/default/types.xml
    rename : collective/pfg/dexterity/profiles/default/types/Dexterity_Content_Adapter.xml => src/collective/pfg/dexterity/profiles/default/types/Dexterity_Content_Adapter.xml
    rename : collective/pfg/dexterity/profiles/default/types/FormFolder.xml => src/collective/pfg/dexterity/profiles/default/types/FormFolder.xml
    rename : collective/pfg/dexterity/profiles/default/workflows.xml => src/collective/pfg/dexterity/profiles/default/workflows.xml
    rename : collective/pfg/dexterity/testing.py => src/collective/pfg/dexterity/testing.py
    rename : collective/pfg/dexterity/tests/__init__.py => src/collective/pfg/dexterity/tests/__init__.py
    rename : collective/pfg/dexterity/tests/test_boolean.py => src/collective/pfg/dexterity/tests/test_boolean.py
    rename : collective/pfg/dexterity/tests/test_feedback.py => src/collective/pfg/dexterity/tests/test_feedback.py

--- i18n.sh
+++ i18n.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-i18ndude rebuild-pot --pot collective/pfg/dexterity/locales/collective.pfg.dexterity.pot --create collective.pfg.dexterity collective/pfg/dexterity
+i18ndude rebuild-pot --pot src/collective/pfg/dexterity/locales/collective.pfg.dexterity.pot --create collective.pfg.dexterity src/collective/pfg/dexterity
 
-i18ndude sync --pot collective/pfg/dexterity/locales/collective.pfg.dexterity.pot collective/pfg/dexterity/locales/*/LC_MESSAGES/collective.pfg.dexterity.po
+i18ndude sync --pot src/collective/pfg/dexterity/locales/collective.pfg.dexterity.pot src/collective/pfg/dexterity/locales/*/LC_MESSAGES/collective.pfg.dexterity.po
