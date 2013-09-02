6797b1b0dabb6cd6f16a9cf9ca5a4c8947605eff
Asko Soukka <asko.soukka@iki.fi>
2013-09-02 06:32:22 +0300

    Preparing release 0.8.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="collective.pfg.dexterity",
-    version='0.7.1.dev0',
+    version='0.8.0',
     description="Installs dexterity content creation adapter for PloneFormGen",
     long_description=(open("README.rst").read() + "\n" +
                       open("CHANGES.txt").read()),
